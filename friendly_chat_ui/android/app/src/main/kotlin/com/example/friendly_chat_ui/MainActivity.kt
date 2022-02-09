de1c53855f40f9fbcf7a3dbbe04b973677f7b4de
SubhamPaul21 <subhampaul12@gmail.com>
2022-02-09 23:08:25 +0530

    add friendly chat UI init files

---
+++ friendly_chat_ui/android/app/src/main/kotlin/com/example/friendly_chat_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.friendly_chat_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ friendly_chat_ui/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Friendly Chat',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Friendly Chat'),
+        ),
+      ),
+    ),
+  );
+}
