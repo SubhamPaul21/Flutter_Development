0aae418b7a1231e47e4dd5476ba03c4a1d409256
SubhamPaul21 <subhampaul12@gmail.com>
2022-02-09 15:05:59 +0530

    feat: Add List View for Text Tile

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -4,23 +4,8 @@ import 'package:startup_namer/util/random_word.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: MyApp(),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Startup Name Generator"),
-      ),
-      body: const Center(
-        child: RandomWords(),
-      ),
-    );
-  }
 }
--- startup_namer/lib/util/random_word.dart
+++ startup_namer/lib/util/random_word.dart
@@ -9,9 +9,43 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _wordSuggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildRow(WordPair wordPair) {
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, i) {
+        // if current index is odd, create a divider i.e. adding divider in each row
+        if (i.isOdd) {
+          return const Divider();
+        }
+        final index = i ~/ 2;
+        // if current index divided by 2 gives an integer which is greater than the number of items in word suggestion list, generate 10 more words and store in the word suggestion list
+        if (index >= _wordSuggestions.length) {
+          _wordSuggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_wordSuggestions[index]);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _wordPair = WordPair.random();
-    return Text(_wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
